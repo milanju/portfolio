39695d064f69b5d2388b21c1cbf8fd5913826e17
unknown <milan.jurhan@live.de>
2015-10-25 18:38:33 +0100

    Initial Commit

---
+++ pages.js
@@ -0,0 +1,15 @@
+var showPage = function (event, page) {
+  event.preventDefault();
+  $('body').css('overflow-y', 'hidden');
+  $('.page-' + page).addClass('show');
+  $('.header').addClass('header-inverted');
+}
+
+var closePages = function (event) {
+  if (event) {
+    event.preventDefault();
+  }
+  $('body').css('overflow-y', 'auto');
+  $('.header').removeClass('header-inverted');
+  $('.page').removeClass('show');
+}
